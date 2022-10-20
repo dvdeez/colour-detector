c1f87e0373bdfffafd3dd10d20f991616329af47
Ruan Alberts <ruan.alberts@outlook.com>
2022-10-20 11:26:56 +0800

    modified to work with 1080p monitor and protanopia

--- colour_detector.py
+++ colour_detector.py
@@ -9,13 +9,14 @@ import time # used to see performance of program
 
 while True:
                         # bbox format = x_start, y_start, x_end, y_end
-    image = ImageGrab.grab(bbox=(250,200,260,210))
+    image = ImageGrab.grab(bbox=(955,535,965,545))
     for y in range(0,10):
         for x in range(0,10):
             colour = image.getpixel((x,y))
-            if colour == (255, 255, 84, 255):
-                print("ligma")
-            else:
-                print("gay")
+            r, g, b = colour[0], colour[1], colour[2]
+            if 215 < r > 255 and 215 < g > 255 and 0 < b > 80:
+                gui.press(".")
+                break
+            
 
     # print("balls") # used to see how many times it checks ALL pixels/second
