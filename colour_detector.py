af0eda706763e084519c364cfd4e9f183fd3e4d0
Ruan Alberts <ruan.alberts@outlook.com>
2022-10-25 14:25:35 +0800

    works for both common ways of coming to stop (letting go and pressing opposing keys

--- colour_detector.py
+++ colour_detector.py
@@ -1,5 +1,6 @@
 from PIL import ImageGrab
 import pyautogui as gui
+import keyboard
 import time # used to see performance of program
 
 # gui.moveTo(250,200)
@@ -15,8 +16,13 @@ while True:
             colour = image.getpixel((x,y))
             r, g, b = colour[0], colour[1], colour[2]
             if 215 < r > 255 and 215 < g > 255 and 0 < b > 80:
-                gui.press(".")
-                break
+                if keyboard.is_pressed('w') == False and keyboard.is_pressed('s') == False:
+                    if keyboard.is_pressed('a') == False and keyboard.is_pressed('d') == False:
+                        gui.press(".")
+                        break
+                    if keyboard.is_pressed('a') == True and keyboard.is_pressed('d') == True:
+                        gui.press(".")
+                        break
             
 
     # print("balls") # used to see how many times it checks ALL pixels/second
