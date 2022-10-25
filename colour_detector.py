96e1f607866ed725a0f2f943d8728bb1dedd3881
Ruan Alberts <ruan.alberts@outlook.com>
2022-10-25 14:29:53 +0800

    gracefully terminate program

--- colour_detector.py
+++ colour_detector.py
@@ -9,22 +9,25 @@ import time
 # while end_time > time.time():
 
 while True:
-                        # bbox format = x_start, y_start, x_end, y_end
-    image = ImageGrab.grab(bbox=(955,535,965,545))
-    for y in range(0,10):
-        for x in range(0,10):
-            colour = image.getpixel((x,y))
-            r, g, b = colour[0], colour[1], colour[2]
-            if 215 < r > 255 and 215 < g > 255 and 0 < b > 80:
-                if keyboard.is_pressed('w') == False and keyboard.is_pressed('s') == False:
-                    if keyboard.is_pressed('a') == False and keyboard.is_pressed('d') == False:
-                        gui.press(".")
-                        time.sleep(0.2)
-                        break
-                    if keyboard.is_pressed('a') == True and keyboard.is_pressed('d') == True:
-                        gui.press(".")
-                        time.sleep(0.2)
-                        break
-            
+    try:
+                            # bbox format = x_start, y_start, x_end, y_end
+        image = ImageGrab.grab(bbox=(955,535,965,545))
+        for y in range(0,10):
+            for x in range(0,10):
+                colour = image.getpixel((x,y))
+                r, g, b = colour[0], colour[1], colour[2]
+                if 215 < r > 255 and 215 < g > 255 and 0 < b > 80:
+                    if keyboard.is_pressed('w') == False and keyboard.is_pressed('s') == False:
+                        if keyboard.is_pressed('a') == False and keyboard.is_pressed('d') == False:
+                            gui.press(".")
+                            time.sleep(0.2)
+                            break
+                        if keyboard.is_pressed('a') == True and keyboard.is_pressed('d') == True:
+                            gui.press(".")
+                            time.sleep(0.2)
+                            break
+    except KeyboardInterrupt:
+        print("\n---PROGRAM TERMINATED---")
+        quit()
 
     # print("balls") # used to see how many times it checks ALL pixels/second
